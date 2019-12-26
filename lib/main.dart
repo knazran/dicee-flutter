8052e73b22ac516ec3466abefa82d1ef342f0cb5
Knazran <khairulnazran94@gmail.com>
2019-12-26 16:03:53 +0800

    Implemented the layout for the dice

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,24 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                print("Left: Pressed")
+              },
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice2.png'),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
